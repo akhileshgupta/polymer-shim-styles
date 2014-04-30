e0ace406fcb2d58f059e2b89c1c3b59a99a6bb25
Akhilesh Gupta <agupta@salesforce.com>
2014-04-30 10:29:33 -0700

    updating shadow selector rules

--- shim-styles.js
+++ shim-styles.js
@@ -11,23 +11,34 @@ var shimRules = function(origRules) {
     var rule = origRules[ruleIdx];
 
     if (rule.type === 'rule' && rule.selectors[0] !== '@font-face') {
-      // Define a polyfill rule as a comment
-      var polyfillRule = {
-        type: 'comment',
-        comment: " @polyfill "
-      };
+
+      var selectors = [];
 
       for (var idx in rule.selectors) {
         var selector = rule.selectors[idx];
         // No need for selector if the selector is body or html
-        rule.selectors[idx] = (selector === 'body' || selector === 'html') ? ':scope' : (':scope /shadow-deep/ ' + selector);
-        polyfillRule.comment += (rule.selectors[idx].replace(':scope', ':host') + ", ");
+        selector = (selector === 'body' || selector === 'html') ? ':scope' : (':scope /deep/ ' + selector);
+        selectors.push(selector);
+        selectors.push(selector.replace(/:scope/, ':host'));
       }
+      rule.selectors = selectors;
+
+      // Create polyfill rule declarations, starting with content property
+      // Define a polyfill rule
+      /*var polyfillRule = {
+        type: 'rule',
+        selectors: ['polyfill-rule'],
+      };
+      polyfillRule.declarations = [].concat({
+        type: "declaration",
+        property: "content",
+        value: ("'" + rule.selectors + "'").replace(/:scope/g, ':host')
+      }, rule.declarations);
 
       // Replace the last comma and space from the comment
-      polyfillRule.comment = polyfillRule.comment.replace(/, $/, ' ');
+      //polyfillRule.comment = polyfillRule.comment.replace(/, $/, ' ');
       // Push polyfill comment and the updated rule
-      rules.push(polyfillRule);
+      rules.push(polyfillRule);*/
     }
     // If rule is a media query, then do a recursive shimming of its rules.
     else if (rule.type === 'media') {
