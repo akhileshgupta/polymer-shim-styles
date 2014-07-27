84234bddb00862062fe55911a1dc1ab7dda93296
Akhilesh Gupta <agupta@salesforce.com>
2014-07-27 15:30:02 -0700

    updating shim rules for native shadow dom and polyfill

--- shim-styles.js
+++ shim-styles.js
@@ -13,32 +13,39 @@ var shimRules = function(origRules) {
     if (rule.type === 'rule' && rule.selectors[0] !== '@font-face') {
 
       var selectors = [];
+      var polyfillRuleSelectors = [];
 
       for (var idx in rule.selectors) {
         var selector = rule.selectors[idx];
         // No need for selector if the selector is body or html
-        selector = (selector === 'body' || selector === 'html') ? ':scope' : (':scope /deep/ ' + selector);
+        if (selector === 'body' || selector === 'html') {
+          selector = ':host';
+        } else {
+          selector = (':host /deep/ ' + selector);
+          selectors.push(selector.replace(':host /deep/', '::content'));
+          selectors.push(selector.replace(':host /deep/', '::content>* /deep/'));
+        }
         selectors.push(selector);
-        selectors.push(selector.replace(/:scope/, ':host'));
+        polyfillRuleSelectors.push(selector);
       }
       rule.selectors = selectors;
 
       // Create polyfill rule declarations, starting with content property
       // Define a polyfill rule
-      /*var polyfillRule = {
+      var polyfillRule = {
         type: 'rule',
-        selectors: ['polyfill-rule'],
+        selectors: ['polyfill-next-selector'],
       };
-      polyfillRule.declarations = [].concat({
+      polyfillRule.declarations = [{
         type: "declaration",
         property: "content",
-        value: ("'" + rule.selectors + "'").replace(/:scope/g, ':host')
-      }, rule.declarations);
+        value: ("'" + polyfillRuleSelectors + "'")
+      }];
 
       // Replace the last comma and space from the comment
       //polyfillRule.comment = polyfillRule.comment.replace(/, $/, ' ');
       // Push polyfill comment and the updated rule
-      rules.push(polyfillRule);*/
+      rules.push(polyfillRule);
     }
     // If rule is a media query, then do a recursive shimming of its rules.
     else if (rule.type === 'media') {
